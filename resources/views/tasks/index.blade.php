5c487c49a7f1ec0afd54d1aba51198fe1fb7a133
Kazuya-F <k.ktm0813@icloud.com>
2019-12-20 15:08:16 +0000

    id_link_show

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -12,18 +12,12 @@
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
-                    <td>{{ $task->id }}</td>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
-        @foreach ($tasks as $task)
-            <tr>
-                <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                <td>{{ $task->content }}</td>
-            </tr>
-        @endforeach
     @endif
     {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
 
